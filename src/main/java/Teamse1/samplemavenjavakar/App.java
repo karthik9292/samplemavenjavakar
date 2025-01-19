ab1fef012c396ecf3a2f0b506eb1b21f7e1b65ea
karthik9292 <karthikprince088@gmail.com>
2025-01-19 22:53:46 +0530

    samplemavenjava

---
+++ src/main/java/Teamse1/samplemavenjavakar/App.java
@@ -0,0 +1,13 @@
+package Teamse1.samplemavenjavakar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
